3d764447e0a23259d2cb30eeefe20f9924c3ed2e
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-21 12:17:09 -0500

    send sms

--- Contacts.jsx
+++ Contacts.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import * as ContactsExpo from 'expo-contacts'
+import * as SMS from 'expo-sms'
 import { View } from 'react-native'
 
 const Contacts = () => {
   const [contacts, setContacts] = useState([]) // List from user's device
-  const [selected, setSelected] = useState([])
+  const [selected, setSelected] = useState(null)
 
   // Get contacts on load
   useEffect(() => {
@@ -19,6 +20,12 @@ const Contacts = () => {
           fields: [ContactsExpo.Fields.PhoneNumbers]
         })
 
+        const alejandro = data.filter(
+          (contact) => contact.firstName === 'Duran'
+        )
+
+        setSelected(alejandro[0].phoneNumbers[0].number)
+
         setContacts(data)
       }
     }
@@ -27,7 +34,25 @@ const Contacts = () => {
 
     return
   }, [])
-  console.log(contacts[0])
+
+  useEffect(() => {
+    if (selected) {
+      const sendSMS = async () => {
+        const isAvailable = await SMS.isAvailableAsync()
+        console.log(isAvailable)
+
+        if (isAvailable) {
+          const { result } = await SMS.sendSMSAsync([selected], 'Que pedos')
+          console.log(result)
+        } else {
+          console.log('Unable to send sms')
+        }
+      }
+      sendSMS()
+    }
+  }, [selected])
+
+  // Send sms draft function
 
   // Handle add to selected list
   const handleAdd = (contact) => setSelected((prev) => [...prev, contact])
