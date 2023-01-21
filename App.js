078c132ca862ac69a49446056c0cff688d2638cd
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-21 11:44:08 -0500

    contacts draft get

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Contacts from './Contacts'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working onasdas your app!</Text>
-      <StatusBar style="auto" />
+      <Contacts />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
---
+++ Contacts.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import * as ContactsExpo from 'expo-contacts'
+import {
+  Input,
+  IconButton,
+  Checkbox,
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Icon,
+  Center,
+  useToast,
+  NativeBaseProvider
+} from 'native-base'
+import { Text, View } from 'react-native'
+
+const Contacts = () => {
+  const [contacts, setContacts] = useState([]) // List from user's device
+  const [selected, setSelected] = useState([])
+
+  // Get contacts on load
+  useEffect(() => {
+    const getData = async () => {
+      const { status } = await ContactsExpo.requestPermissionsAsync()
+
+      if (status === 'granted') {
+        const { data } = await ContactsExpo.getContactsAsync({
+          fields: [ContactsExpo.Fields.FirstName],
+          fields: [ContactsExpo.Fields.LastName],
+          fields: [ContactsExpo.Fields.Emails],
+          fields: [ContactsExpo.Fields.PhoneNumbers]
+        })
+
+        setContacts(data)
+      }
+    }
+
+    getData()
+
+    return
+  }, [])
+  console.log(contacts[0])
+
+  // Handle add to selected list
+  const handleAdd = (contact) => setSelected((prev) => [...prev, contact])
+
+  // Handle Delete
+  const handleRemove = (id) => {
+    const filtered = selected.filter((contact) => contact.id !== id)
+    setSelected(filtered)
+  }
+
+  return <View></View>
+}
+
+export default Contacts
