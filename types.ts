1b6d37f09a60c2bdbacf2308d48f18e4de9352a5
Alejandro Duran <aledurax@gmail.com>
2023-01-21 14:56:23 -0500

    some state machinse

--- types.ts
+++
@@ -1,4 +0,0 @@
-import { Contact } from "expo-contacts";
-
-type UsableContact = Pick<Contact, 'name' | "phoneNumbers" | 'emails'>;
-export type Item = { contact?: UsableContact; title: string; id: string; completed: boolean; };
